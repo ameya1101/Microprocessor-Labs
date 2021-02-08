07d6933a531140d8391b11058480a1a96200affb
ameya1101 <ameyathete@icloud.com>
2021-02-08 20:55:10 +0530

    Corrections to Lab 2

--- Lab 2/lab202a.asm
+++ Lab 2/lab202a.asm
@@ -1,14 +1,26 @@
 .model tiny
 .486
 .data
-NUMS DW 1234h, 0ABBAh
+NUM1 DW 1h, 2h, 3h, 4h, 5h, 6h, 7h, 8h
+NUM2 DW 0h, 0Ah, 0Bh, 0Ch, 0Dh, 0Eh, 0Fh
+COUNT EQU 8d
 .code
 .startup
-    LEA BX, NUMS
-    MOV AX, [BX]
-    ADD BX, 0002h
-    ADD AX, [BX]
-    ADD BX, 0002h
-    MOV [BX], AX
+        LEA SI, NUM1
+        LEA DI, NUM2
+        MOV CX, COUNT
+        MOV BL, 0
+        CLC
+    X1: MOV AX, [SI]
+        ADC [DI], AX
+        INC SI
+        INC SI
+        INC DI
+        INC DI
+        DEC CX
+        JNZ X1
+        JNC X2
+        INC BL
+    X2: MOV [DI], BL
 .exit
 end
--- Lab 2/lab202b.asm
+++ Lab 2/lab202b.asm
@@ -1,14 +1,30 @@
 .model tiny
 .486
 .data
-NUMS DD 00001234h, 0000ABBAh
+NUM1 DD 1h, 2h, 3h, 4h
+NUM2 DD 0h, 0Ah, 0Bh, 0Ch
+COUNT EQU 4d
 .code
 .startup
-    LEA EBX, NUMS
-    MOV EAX, [EBX]
-    ADD EBX, 0004h
-    ADD EAX, [EBX]
-    ADD EBX, 0004h
-    MOV [EBX], EAX
+        LEA ESI, NUM1
+        LEA EDI, NUM2
+        MOV ECX, COUNT
+        MOV BL, 0
+        CLC
+    X1: MOV AX, [ESI]
+        ADC [EDI], AX
+        INC ESI
+        INC ESI
+        INC ESI
+        INC ESI
+        INC EDI
+        INC EDI
+        INC EDI
+        INC EDI
+        DEC ECX
+        JNZ X1
+        JNC X2
+        INC BL
+    X2: MOV [EDI], BL
 .exit
 end
